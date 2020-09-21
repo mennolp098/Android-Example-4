4dea28f3d7d4b8dbfddbf4a68761005900fe9e79
Menno <run_jongejan@hotmail.com>
2020-09-21 18:15:17 +0200

    Created reminder data and database

---
+++ app/src/main/java/com/example/android_example_4/ReminderDao.kt
@@ -0,0 +1,20 @@
+package com.example.android_example_4
+
+import androidx.room.*
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminderTable")
+    fun getAllReminders(): List<Reminder>
+
+    @Insert
+    fun insertReminder(reminder: Reminder)
+
+    @Delete
+    fun deleteReminder(reminder: Reminder)
+
+    @Update
+    fun updateReminder(reminder: Reminder)
+
+}
